a10a3bd3b461a7fb8fa5d3f0c12b1e1e697ef40a
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-03-19 05:04:04 -0300

    Modificado: Funciones propias de Make sin docker

--- .config/functions.mk
+++ .config/functions.mk
@@ -1,11 +1,11 @@
-define make_cmd
-	make  --no-print-directory -C $(ARGS) $1
+# 1er param: $1: recibe el nombre de una o varias reglas de un makefile
+# 2do param: $2: recibe el nombre de un modulo del proyecto (es opcional)
+define specific_module_cmd
+	$(MAKE) --no-print-directory -C $(DIR_PROJECT)/$2 $1
 endef
 
-define docker_cmd
-	docker $1 $(IMAGE_NAME) $2
-endef
-
-define docker_make_cmd
-	docker exec $(IMAGE_NAME) make --no-print-directory -C $(ARGS) $1
+# 1er param: $1: recibe el nombre de una o varias reglas de un makefile
+# ARGS: recibe multiples parametros por la terminal
+define module_cmd
+	$(MAKE) --no-print-directory -C $(DIR_PROJECT)/$(ARGS) $1
 endef
