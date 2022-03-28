2f8326bf2b2958a5e9dba9a74298aa6f8c2d6167
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-03-28 03:38:19 -0300

    Modificado: Includes y estructura para soportar nuevas static/shared libraries

---
+++ project/shared/include/libshared.h
@@ -0,0 +1,5 @@
+#ifndef __MY_SHARED_LIB_H__
+#define __MY_SHARED_LIB_H__
+
+void saludar(void);
+#endif
---
+++ project/static/src/libstatic.c
@@ -0,0 +1,6 @@
+#include "libstatic.h"
+#include <stdio.h>
+
+void saludar(void) {
+  printf("Hola..!\n");
+}
