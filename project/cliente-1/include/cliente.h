97bec7994c47481dd3b6127a3a81ab4c941098ab
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-04-21 02:47:11 -0300

    Añadido: Parseo de archivo de instrucciones en Cliente-1

--- project/cliente-1/include/cliente.h
+++ project/cliente-1/include/cliente.h
@@ -13,8 +13,10 @@
 t_log *logger;
 
 #define MODULO "cliente-1"
+#define DIR_CONFIG DIR_BASE MODULO "/config"
 #define DIR_LOG_MESSAGES DIR_BASE MODULO "/logs/messages.log"
 #define DIR_CLIENTE_CFG DIR_BASE MODULO "/config/cliente.cfg"
 
 int conectarse_a_kernel();
+t_list* obtener_instrucciones_de_archivo(char* ruta_archivo);
 #endif
--- project/cliente-1/src/cliente.c
+++ project/cliente-1/src/cliente.c
@@ -10,25 +10,38 @@
 
 t_config* config;
 
-int main() {
+int main(int argc, char* argv[]) {
   logger = iniciar_logger(DIR_LOG_MESSAGES, "Cliente-1");
   config = iniciar_config(DIR_CLIENTE_CFG);
 
-  int fd_kernel = conectarse_a_kernel();
+  char path_archivo_instrucciones[100];
+  int tamanio_proceso = 0;
 
-  t_list* lista_instrucciones = list_create();
-  list_add(lista_instrucciones, instruccion_create("NO_OP", "3000"));
-  list_add(lista_instrucciones, instruccion_create("WRITE", "4 42"));
-  list_add(lista_instrucciones, instruccion_create("READ", "9"));
+  if (!argv[1]) {
+    log_error(logger, "se requieren dos parámetros por la consola (ruta archivo de instrucciones + tamanio archivo)");
+  } else {
+    if (!argv[2]) {
+      log_error(logger, "se requiere un segundo parámetro, el tamanio archivo)");
+    }
 
+    strcpy(path_archivo_instrucciones, argv[1]);
+
+    tamanio_proceso = atoi(argv[2]);
+  }
+
+  // t_list* lista_instrucciones = obtener_instrucciones_de_archivo(DIR_CONFIG "/instrucciones.txt");
+  t_list* lista_instrucciones = obtener_instrucciones_de_archivo(path_archivo_instrucciones);
   t_pcb* pcb = pcb_fake();
   pcb->tamanio = 11111; // TODO: debe ser información recibida por la terminal
-  pcb->instrucciones = lista_instrucciones; // TODO: debe ser info parseada de un archivo config
+  pcb->instrucciones = lista_instrucciones;
+
   t_paquete* paquete_con_pcb = paquete_create();
   paquete_add_pcb(paquete_con_pcb, pcb);
 
+  int fd_kernel = conectarse_a_kernel();
   enviar_pcb(fd_kernel, paquete_con_pcb);
 
+  pcb_destroy(pcb);
   paquete_destroy(paquete_con_pcb);
 
   terminar_cliente(fd_kernel, logger, config);
@@ -42,4 +55,27 @@ int conectarse_a_kernel() {
   int fd_servidor = conectar_a_servidor(ip, puerto);
 
   return fd_servidor;
+}
+
+t_list* obtener_instrucciones_de_archivo(char* ruta_archivo) {
+  int buffer_tamanio = 255;
+  char buffer[buffer_tamanio];
+  FILE* archivo_con_instrucciones = fopen(ruta_archivo, "r");
+  t_list* lista_instrucciones = list_create();
+
+  while (fgets(buffer, buffer_tamanio, archivo_con_instrucciones)) {
+    buffer[strcspn(buffer, "\n")] = 0; // removemos los saltos de linea
+
+    char** instruccion_texto = string_n_split(buffer, 2, " ");
+    char* identificador = instruccion_texto[0];
+    char* params = instruccion_texto[1] ? instruccion_texto[1] : "";
+    t_instruccion* instruccion = instruccion_create(identificador, params);
+    string_array_destroy(instruccion_texto);
+
+    list_add(lista_instrucciones, instruccion);
+  }
+
+  fclose(archivo_con_instrucciones);
+
+  return lista_instrucciones;
 }
