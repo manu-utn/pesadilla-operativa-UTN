e4e224aa93045df8853d8e0d191190758ecd8514
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-06-17 19:52:23 -0300

    Fix: Memoria, estructuras de las tablas de paginas 1er/2do  nivel

--- project/memoria/include/memoria.h
+++ project/memoria/include/memoria.h
@@ -1,6 +1,7 @@
 #ifndef MEMORIA_H_
 #define MEMORIA_H_
 
+#include <commons/collections/dictionary.h>
 #include <commons/collections/list.h>
 #include <commons/config.h>
 #include <commons/log.h>
@@ -17,6 +18,11 @@
 #define MODULO "memoria"
 #define DIR_LOG_MESSAGES DIR_BASE MODULO "/logs/messages.log"
 #define DIR_MEMORIA_CFG DIR_BASE MODULO "/config/configMemoria.cfg"
+
+typedef enum {
+  OPERACION_LECTURA = 1,
+  OPERACION_ESCRITURA = 2
+} t_operacion_memoria;
 
 typedef struct{
     int puerto_escucha;
@@ -41,7 +47,7 @@ typedef struct{
 typedef struct{
     int num_tabla;
     int pid;
-    t_list* entradas;
+    t_dictionary* entradas_primer_nivel;
 }t_tabla_primer_nivel;
 
 
@@ -53,7 +59,7 @@ typedef struct{
 typedef struct{
     int num_tabla;
     int pid;
-    t_list* entradas;
+    t_dictionary* entradas_segundo_nivel;
 }t_tabla_segundo_nivel;
 
 typedef struct{
@@ -95,16 +101,25 @@ void* manejar_nueva_conexion(void* args);
 
 //Estructura admin de paginas y marcos
 t_list* tabla_marcos;
-t_dictionary* diccionario_tablas;
-t_list* lista_tablas_segundo_nivel;
+// TODO: cambiar a tablas_TP_primer_nivel
+// t_dictionary* diccionario_tablas;
+t_dictionary* tablas_de_paginas_primer_nivel;
+
+// t_list* lista_tablas_segundo_nivel;
+t_dictionary* tablas_de_paginas_segundo_nivel;
+
 t_list* marcos_prueba_clock;
 int buscar_marco_libre();
 int generar_numero_tabla();
-void inicializar_proceso(int pid, int entradas_por_tabla, int tam_proceso);
+// void inicializar_proceso(int pid, int entradas_por_tabla, int tam_proceso);
+void inicializar_estructuras_de_este_proceso(int pid, int tam_proceso);
 int inicializar_tabla_marcos();
 void mostrar_tabla_marcos();
 uint32_t buscar_dato_en_memoria(uint32_t dir_fisica);
-int buscar_tabla_segundo_nivel(int num_tabla_primer_nivel,int entrada_primer_nivel);
+
+// int buscar_tabla_segundo_nivel(int num_tabla_primer_nivel,int entrada_primer_nivel);
+int obtener_numero_TP_segundo_nivel(int numero_TP_primer_nivel, int entrada_tabla);
+
 int obtener_marco(int num_tabla_segundo_nivel,int entrada_segundo_nivel, int operacion);
 void llenar_memoria_mock();
 t_tabla_primer_nivel* encontrar_tabla(int pid);
@@ -115,4 +130,10 @@ int ejecutar_reemplazo(int pid, t_entrada_tabla_segundo_nivel* entrada);
 t_entrada_tabla_segundo_nivel* ejecutar_clock(t_list* marcos, t_entrada_tabla_segundo_nivel* entrada);
 
 void liberar_estructuras_en_swap();
+int entrada_TP_2do_nivel_marco_asignado(int num_tabla_segundo_nivel, int entrada_segundo_nivel);
+
+int obtener_cantidad_entradas_por_tabla_por_config();
+t_tabla_primer_nivel* tabla_paginas_primer_nivel_create();
+t_tabla_segundo_nivel* tabla_paginas_segundo_nivel_create(int numero_tabla_segundo_nivel);
+void inicializar_entrada_de_tabla_paginas(t_entrada_tabla_segundo_nivel entrada_tabla_segundo_nivel);
 #endif /* MEMORIA_H */
